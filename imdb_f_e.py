0311edc661fe3f30ddc509f51de0d047a9221bc3
ozkanyazici1992 <58921061+ozkanyazici1992@users.noreply.github.com>
2025-08-15 08:58:55 +0300

    Update imdb_f_e.py

--- imdb_f_e.py
+++ imdb_f_e.py
@@ -41,7 +41,7 @@ def normalize_title(title):
         if unicodedata.category(c) != 'Mn'
     ).lower().strip()
 
-def prepare_data(filepath="others/movies_imdb.csv", vote_threshold=10, min_votes=2500):
+def prepare_data(filepath="others/movies_imdb.csv", vote_threshold=10, min_votes=250):
     """
     Prepare the dataset for recommendations:
 
@@ -376,3 +376,4 @@ and user experience. This was especially helpful for users to easily distinguish
   * **IMDB\_SCORE:** Weighted IMDb score
 
 """
+
